c8bacb616c34f05bea169e402f312b8d47729399
Subilan <suyuanliang1995@gmail.com>
2023-10-26 17:57:10 +0800

    refactor: training

--- train.py
+++ train.py
@@ -1,47 +1,91 @@
 import os
 import openai
 from sys import argv
+from argparse import ArgumentParser
 
-assert len(argv) >= 1
+apikey = os.getenv("OPENAI_API_KEY")
+
+if apikey == None:
+    print("Warning: Missing required enviromental variable $OPENAI_API_KEY. Calling to OpenAI API won't work properly.")
+
+parser = ArgumentParser(
+    prog="train",
+    description="Provide commands that call corresponding OpenAI API with customizable arguments.",
+)
+
+parser.add_argument("command")
+parser.add_argument('-i', '--identifier')
+parser.add_argument('-l', '--limit')
+parser.add_argument('-f', '--filename')
+parser.add_argument('-m', '--model')
+parser.add_argument('-cu', '--contentUser')
+parser.add_argument('-cs', '--contentSystem')
+args = parser.parse_args()
+
+openai.api_key = apikey
 
 def main():
-    if len(argv) == 1:
-        print("Usage: python train.py <command> [arguments]")
-        return
+    match (args.command):
+        case "upload":
+            filename = args.filename
+            if filename == None:
+                print("Missing required argument 'filename'.")
+                return
+            print(openai.File.create(
+                file=open(filename, "rb"),
+                purpose="fine-tune"
+            ))
 
-    cmd = argv[1]
-
-    if cmd == "upload":
-        filename = "data.jsonl" if len(argv) < 3 else argv[2]
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        print(openai.File.create(
-            file=open("data.jsonl", "rb"),
-            purpose="fine-tune"
-        ))
-        
-    if cmd == "create":
-        if len(argv) < 3:
-            print("Missing required argument: file identifier")
-            return
-        filename = argv[2]
-        model = "gpt-3.5-turbo" if len(argv) < 4 else argv[3]
-        print(openai.FineTuningJob.create(training_file=filename, model=model))
-        
-    if cmd == "list":
-        limit = 10 if len(argv) < 3 else int(argv[2])
-        print(openai.FineTuningJob.list(limit=limit))
-
-    if cmd == "retrieve" or cmd == "cancel" or cmd == "list-events":
-        if len(argv) < 3:
-            print("Missing required argument: job identifier")
-            return
-        
-        job = argv[2]
-        
-        if cmd == "retrieve": print(openai.FineTuningJob.retrieve(job))
-        if cmd == "cancel": print(openai.FineTuningJob.cancel(job))
-        if cmd == "list-events":
-            limit = 10 if len(argv) < 4 else argv[3]
-            print(openai.FineTuningJob.list_events(id=job, limit=limit))
+        case "create":
+            fileid = args.identifier
+            model = args.model
+            if fileid == None:
+                print("Missing required argument 'identifier'.")
+                return
+            if model == None:
+                print("Missing required argument 'model'.")
+                return
+            print(openai.FineTuningJob.create(training_file=fileid, model=model))
             
+        case "list":
+            limit = int(args.limit) if args.limit != None else None
+            if limit == None:
+                print("Missing optional argument 'limit', default to 10.")
+                limit = 10
+            print(openai.FineTuningJob.list(limit=limit))
+            
+        case "retrieve" | "cancel" | "list-events":
+            jobid = args.identifier
+            limit = int(args.limit) if args.limit != None else None
+            if jobid == None:
+                print("Missing required argument 'identifier'.")
+                return
+            match args.command:
+                case "retrieve":
+                    print(openai.FineTuningJob.retrieve(jobid))
+                
+                case "cancel":
+                    print(openai.FineTuningJob.cancel(jobid))
+                    
+                case "list-events":
+                    if limit == None:
+                        print("Missing optional argument 'limit', default to 10.")
+                        limit = 10
+                    print(openai.FineTuningJob.list_events(id=jobid, limit=limit))
+
+        case "create-completion":
+            model = args.model
+            user_content = args.contentUser
+            system_content = args.contentSystem
+            completion = openai.ChatCompletion.create(
+                model=model,
+                messages=[
+                    {"role": "system", "content": system_content or ""},
+                    {"role": "user", "content": user_content or ""}
+                ]
+            )
+            print(completion)
+
+        case _:
+            parser.print_help()
 main()
