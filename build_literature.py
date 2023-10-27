10d5383bd0dd04d903721de5771d41c4fd6c5811
Subilan <suyuanliang1995@gmail.com>
2023-10-27 12:25:34 +0800

    build literature script

---
+++ build_literature.py
@@ -0,0 +1,41 @@
+import re
+import nltk
+from build_vocabulary import based, toJsonLine
+from os import remove
+
+with open("translations/literature.txt", mode='r+') as literature:
+    lines = literature.readlines()
+    withoutReturn = [line for line in lines if line != '\n']
+    separated = []
+    index = 0
+    for line in withoutReturn:
+        parts = []
+        # if (index % 2 == 0): parts = nltk.tokenize.sent_tokenize(line) re.findall("\w{3,}\.", line)
+        # Note: NLTK cannot process abbrs. such as pp. well.
+        if (index % 2 == 0):
+            parts = re.split(re.compile("\w{3,}）?\.\s"), line)
+            i = 0
+            for word in re.findall("\w{3,}）?\.\s", line):
+                 parts[i] += word
+                 i += 1
+        else:
+            parts = line.split("。")
+        separated.append(parts)
+        index += 1
+        
+    remove("literature.jsonl")
+    
+    with open("literature.jsonl", mode='a') as separated_literature:
+        index = 0
+        for s in separated:
+            if index < len(separated):
+                currentTargetEnglish = separated[index]
+                currentTargetChinese = separated[index + 1]
+                for i in range(len(currentTargetEnglish) - 1): # minus 1 to adapt array index expression (0~len-1)
+                    # separated_literature.write(currentTargetEnglish[i])
+                    # separated_literature.write(currentTargetChinese[i])
+                    separated_literature.write(toJsonLine(based(
+                        "Please translate '{0}' into Chinese accurately.".format(currentTargetEnglish[i]),
+                        currentTargetChinese[i]
+                    )))
+            index += 2
