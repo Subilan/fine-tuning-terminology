46c925bf5861063dad12bf0a7cccb83bea758efc
Subilan <suyuanliang1995@gmail.com>
2023-10-31 17:39:14 +0800

    generate questions without template

--- fn.py
+++ fn.py
@@ -1,14 +1,38 @@
 from json import dumps
 
-def withRoles(question, answer):
+_default_system_message__old = "Linga is a psychologist chatbot that can accurately translate the terminologies in literature in his area to Chinese."
+default_system_message = 'You are a psychologist chatbot that can accurately translate the terminologies in literature in his area to Chinese.'
+default_prompt_template = 'Now I will give you a paragraph of a psychological literature. Please focus on the psychological term and translate the everything in the paragraph into Chinese accurately and make the content more understandable and in line with the Chinese expression system, without changing the meaning of the original text. Please give me the best result: {0}'
+
+def buildPrompt(question):
     return [
-        {"role": "system", "content": "Linga is a psychologist chatbot that can accurately translate the terminologies in literature in his area to Chinese."},
+        {"role": "system", "content": default_system_message},
+        {"role": "user", "content": question}
+    ]
+
+def buildLine(question, answer):
+    return [
+        {"role": "system", "content": default_system_message},
         {"role": "user", "content": question},
         {"role": "assistant", "content": answer}
     ]
     
-def toJsonl(obj):
+def dumpSimple(obj):
+    return dumps(obj, ensure_ascii=False)
+    
+def dumpLine(obj):
     return dumps({"messages": obj}, ensure_ascii=False) + "\n"
 
 def removeReturn(string):
     return string.replace('\n', '')
+
+def withoutReturn(array):
+    return [x for x in array if x != '\n']
+
+def splitByRegex(string, regex):
+    parts = re.split(re.compile(regex), string)
+    i = 0
+    for word in re.findall(regex, string):
+        parts[i] += word
+        i += 1
+    return parts
--- generate_questions.py
+++ generate_questions.py
@@ -1,5 +1,3 @@
-from fn import default_prompt_template
-
 lines = open("translations/separated.txt", mode='r').readlines()
 
 lines_to_write = []
@@ -7,7 +5,7 @@ lines_to_write = []
 index = 0
 for l in lines:
     if index % 2 == 0:
-        lines_to_write.append(default_prompt_template.format(l))
+        lines_to_write.append(l)
     index += 1
 
 open("to_translate.txt", mode='w').writelines(lines_to_write)
