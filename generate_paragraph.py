aaf7de28726790209f65c67ff43ab8378abb9207
Subilan <suyuanliang1995@gmail.com>
2023-11-04 20:08:17 +0800

    overwrite

--- generate_paragraph.py
+++ generate_paragraph.py
@@ -1,6 +1,12 @@
 from fn import dumpLine, buildLine
+from os import remove
 
 lines = open("translations/literature.txt", mode='r').readlines()
+
+try:
+    remove("paragraphs.jsonl")
+except:
+    pass
 
 with open("paragraphs.jsonl", mode='a+') as paragraphs:
     for i in range(len(lines)):
