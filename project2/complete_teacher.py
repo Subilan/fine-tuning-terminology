23555e3073dfdf725cf5684fac5ea625303775da
Subilan <suyuanliang1995@gmail.com>
2023-11-08 12:14:01 +0800

    teacher

--- project2/complete_teacher.py
+++ project2/complete_teacher.py
@@ -5,7 +5,8 @@ from models import latest_teacher_model
 
 parser = argparse.ArgumentParser()
 
+parser.add_argument('-m', '--model')
 parser.add_argument('question')
 args = parser.parse_args()
     
-subprocess.run(f'python3 ./complete.py "{args.question:s}" --model {latest_teacher_model:s}', shell=True, encoding='utf-8')
+subprocess.run(f'python3 ./complete.py "{args.question:s}" --model {latest_teacher_model if args.model == None else args.model:s}', shell=True, encoding='utf-8')
